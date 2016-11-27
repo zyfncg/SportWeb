4f5dcc9d7f936588e50e3948d55d643d673291f2
zyfncg <1370305206@qq.com>
2016-11-27 16:45:27 +0800

    Ｏ(≧口≦)Ｏ

--- src/php/businesslogic/checklogin.php
+++ src/php/businesslogic/checklogin.php
@@ -10,12 +10,11 @@ session_start();
 $username = htmlspecialchars($_POST['username']);
 //$password = md5($_POST['password']);
 $password = $_POST['password'];
-echo $username;
-echo "<br>";
-echo $password;
+//echo $username;
+//echo "<br>";
+//echo $password;
 //包含数据库连接文件
 include '../database/database.php';
-
 $db = DB::getInstance();
 $sql = "select * from user where name = '$username' and password = '$password'";
 
@@ -26,11 +25,12 @@ if($result = $check_query->fetchArray(SQLITE3_ASSOC)){
 //    $_SESSION['userid'] = $result['userid'];
     echo $username,' 欢迎你！进入 <a href="my.php">用户中心</a><br />';
     echo '点击此处 <a href="login.php?action=logout">注销</a> 登录！<br />';
-//    header('Location: ../index.html');
+    header('Location: ../../html/mySport.html');
     exit;
 }else {
     $doc = array("error" => "Username or Password error");
     echo json_encode($doc);
+    header('Location: ../../html/login1.html');
 }
 
 
